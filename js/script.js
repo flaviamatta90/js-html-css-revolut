3ec219a6ad0ee2a0cf8f3a25c5121a54742f9209
Flavia <flaviamatta90@gmail.com>
2020-09-03 20:03:39 +0200

    mouseover mouseout

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(
+  function (){
+
+$(".open").mouseover(
+  function(){
+    $(this).children(".lista").toggle()
+
+  }
+);
+$(".open").mouseout(
+  function(){
+    $(this).children(".lista").toggle()
+
+  }
+);
+
+
+}
+);
